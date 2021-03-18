2d573541bc2614ab9539c6f4d02e5cdbcc71b882
Aditya Shah <nischalshah1999@gmail.com>
2021-03-18 20:01:51 +0545

    Activity and Lifecycle completed.

--- app/src/main/java/com/aditya/android_todo_first/MainActivity.java
+++ app/src/main/java/com/aditya/android_todo_first/MainActivity.java
@@ -12,36 +12,58 @@ public class MainActivity extends AppCompatActivity {
 
     private String[] mTodos;
     private int mTodoIndex = 0;
+    private TextView TodoTextView;
+    /* In case of state change, such as rotating the phone,
+       store the mTodoIndex */
+    private static final String TODO_INDEX = "com.example.android_todo_first.todoIndex";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
         super.onCreate(savedInstanceState);
-
-        // set the user interface layout for this Activity
-        setContentView(R.layout.activity_main);
-
-        // initialize member TextView so we can manipulate it later
-        final TextView TodoTextView;
-        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
-
-        // read the todo array from res/values/strings.xml
         Resources res = getResources();
         mTodos = res.getStringArray(R.array.todos);
-
-        // display the first task from mTodo array in the TodoTextView
+        setContentView(R.layout.activity_todo);
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
         TodoTextView.setText(mTodos[mTodoIndex]);
 
+        /* setup navigation buttons */
+        /* next button to cycle through mTodos */
         Button buttonNext;
         buttonNext = (Button) findViewById(R.id.buttonNext);
 
-        // OnClick listener for the  Next button
-        buttonNext.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                mTodoIndex += 1;
-                TodoTextView.setText(mTodos[mTodoIndex]);
-            }
-        });
+        /* OnClick listener for the  Next button */
+        buttonNext.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+
+                        //check index boundary for mTodos
+                        if (mTodoIndex < mTodos.length - 1) {
+                            mTodoIndex += 1;
+                        } else {
+                            mTodoIndex = 0;
+                        }
+
+
+                        //   mTodoIndex += 1;
+                        TodoTextView.setText(mTodos[mTodoIndex]);
+                    }
+                });
+
     }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        mTodoIndex = savedInstanceState.getInt(TODO_INDEX);
+        TodoTextView.setText(mTodos[mTodoIndex]);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
 }
